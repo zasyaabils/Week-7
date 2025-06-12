c91d0f09233e80f4aba27c03bec93df855ac7252
Zahra Salsabila <2400018206@webmail.uad.ac.id>
2025-06-12 15:03:20 +0700

    Add files via upload

---
+++ studikas7.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+using namespace std;
+
+class Karyawan {
+private:
+	int id, notelp;
+	int n;
+	string nama, jabatan;
+	
+public:
+	void input() {
+		
+		cout << "\nMasukkan jumlah data : ";
+		cin >> n;
+		
+		for (int i = 0; i < n; i++) {
+			cout << "\nMasukkan Data ke-" << i+1 << endl;
+			cout << "Masukkan ID		: ";
+			cin >> id[i];
+			cout << "Masukkan Nama		: ";
+			cin >> nama[i];
+			cout << "Masukkan Jabatan	: ";
+			cin >> jabatan[i];
+			cout << "Masukkan No Telp	: ";
+			cin >> notelp[i];
+			cout << endl;
+		}
+		
+	}
+	
+	void tampil() {
+		cout << "\n-----------------------------\n";
+		cout << "	Data Karyawan\n";
+		cout << "-----------------------------\n";
+		for (int i = 0; i < n; i++) {
+			cout << "ID	: " << id[i] << endl;
+			cout << "Nama	: " << nama[i] << endl;
+			cout << "Jabatan	: " << jabatan[i] << endl;
+			cout << "No Telp	: " << notelp[i] << endl;
+			cout << endl;
+		}
+		cout << "-----------------------------\n\n";
+		
+	}
+	
+	void ascending() {
+		cout << "\n-----------------------------\n";
+		cout << "	Data Diurutkan berdasarkan Asceding\n";
+		cout << "-----------------------------\n";
+		for (int i = 0; i < n-1; i++) {
+				for (int j = 0; j < n-i-1; j++) {
+					if (id[j] > id[j + 1]) {
+						// Tukar Data
+						int temp = id[j];
+						id[j] = id[j + 1];
+						id[j + 1] = temp;
+					}
+				}
+			}
+	}
+	
+	void descending() {
+		
+	}
+	
+	void cari() {
+		
+	}
+	
+	void menu() {
+		int pilMenu;
+		
+		do {
+			cout << "-----------------------------\n";
+			cout << "	PT. Quairo\n";
+			cout << "-----------------------------\n";
+			cout << "1. Input Data Karyawan\n";
+			cout << "2. Tampilkan Data Karyawan\n";
+			cout << "3. Sorting Data secara Ascending\n";
+			cout << "4. Sorting Data secara Descending\n";
+			cout << "5. Cari Data berdasarkan ID\n";
+			cout << "6. Keluar\n";
+			cout << "-----------------------------\n";
+			cout << "Pilih Menu : ";
+			cin >> pilMenu;
+			
+			switch(pilMenu) {
+				case 1:
+					input();
+					break;
+				case 2:
+					tampil();
+					break;
+				case 3:
+					ascending();
+					break;
+				case 4:
+					descending();
+					break;
+				case 5:
+					cari();
+					break;
+				default:
+					cout << "Pilihan Anda tidak valid!.\n";
+					break;
+			}
+		} while (pilMenu != 6);
+	}
+};
+
+int main() {
+	Karyawan k;
+	
+	k.menu();
+	
+	return 0;
+}
