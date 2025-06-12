e3273313b6d03695f05beb189fd88468e9f6c3a6
Zahra Salsabila <2400018206@webmail.uad.ac.id>
2025-06-12 15:14:08 +0700

    Update studikas7.cpp

--- studikas7.cpp
+++ studikas7.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 class Karyawan {
 private:
-	int id, notelp;
+	int id[99], notelp[99];
 	int n;
-	string nama, jabatan;
+	string nama[99], jabatan[99];
 	
 public:
 	void input() {
@@ -44,9 +44,9 @@ public:
 	}
 	
 	void ascending() {
-		cout << "\n-----------------------------\n";
+		cout << "\n----------------------------------------\n";
 		cout << "	Data Diurutkan berdasarkan Asceding\n";
-		cout << "-----------------------------\n";
+		cout << "-----------------------------------------\n";
 		for (int i = 0; i < n-1; i++) {
 				for (int j = 0; j < n-i-1; j++) {
 					if (id[j] > id[j + 1]) {
@@ -63,8 +63,26 @@ public:
 		
 	}
 	
-	void cari() {
+	int cari() {
 		
+		int key;
+	    cout << "\nMasukkan ID yang ingin dicari : ";
+	    cin >>key;
+	    
+		int left= 0, right = n - 1;
+			
+			while (left <= right) {
+				int mid = (left + right) / 2;
+				
+				if (id[mid] == key)
+					return mid; // Ditemukan 
+				else if (id[mid] < key)
+					left = mid + 1;
+				else
+					right = mid - 1;
+			}
+			
+			return -1; // Tidak Ditemukan
 	}
 	
 	void menu() {
@@ -101,7 +119,7 @@ public:
 					cari();
 					break;
 				default:
-					cout << "Pilihan Anda tidak valid!.\n";
+					cout << "Terima Kasih!.\n";
 					break;
 			}
 		} while (pilMenu != 6);
